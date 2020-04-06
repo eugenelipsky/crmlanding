6b1d3a9568a20fe43204e726fd0b258fde5acde9
eugenelipsky <zvlipskiy@mail.ru>
2020-04-07 01:09:12 +0300

    Resources section added

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 $(function () {
- $('.header__hero-slide').slick({
-     arrows: false,
-     dots: true
- });
+    $('.header__hero-slide').slick({
+        arrows: false,
+        dots: true
+    });
+
+    $('.resources__slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        centerMode: true,
+        variableWidth: true
+    });
 });
 
